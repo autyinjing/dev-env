9ba02ba53b5a8f43998f8d67d886d843eb56ba2f
aut <aut@localhost.localdomain>
2017-03-03 16:16:26 +0800

    add test

---
+++ cmake/main.cc
@@ -0,0 +1,21 @@
+/* ***********************************************************************
+
+  > File Name: main.cc
+  > Author: Aut
+  > Mail: aut@taomee.com 
+  > Created Time: 2017年03月03日 星期五 13时33分57秒
+
+ ********************************************************************** */
+
+#include <iostream>
+#include "src/Test.h"
+
+extern void print_pmt();
+
+int main(int argc, const char* argv[])
+{
+    print_pmt();
+
+    return 0;
+}
+
---
+++ cmake/src/Test.cc
@@ -0,0 +1,17 @@
+/* ***********************************************************************
+
+  > File Name: Test.cc
+  > Author: Aut
+  > Mail: aut@taomee.com 
+  > Created Time: 2017年03月03日 星期五 13时36分21秒
+
+ ********************************************************************** */
+
+#include <iostream>
+#include "Test.h"
+
+void print_pmt()
+{
+    std::cout << "Hello World!\n";
+}
+
